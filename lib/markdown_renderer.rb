e076a4e5134abbc7c54a47054f713d4cdec1c16f
Rodrigo Navarro <rnavarro1@gmail.com>
2012-07-11 01:39:14 -0300

    Rescuing from LoadError.

--- lib/markdown_renderer.rb
+++ lib/markdown_renderer.rb
@@ -8,7 +8,7 @@ class MarkdownRenderer < Redcarpet::Render::HTML
   def block_code(code, language)
     begin
       Pygments.highlight code, :lexer => language, :options => { :encoding => "utf-8" }
-    rescue
+    rescue LoadError, StandardError
       Net::HTTP.post_form(URI.parse("http://pygments.appspot.com/"), "code" => code, "lang" => language).body
     end
   end
